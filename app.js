75c91b94c67648791c080a1afbb97383736d4a09
dongyisun <56062169+dongyisun@users.noreply.github.com>
2022-02-06 23:21:04 -0800

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.post('/v1/auth', auth)
 
 
 // adding token for two endpoints
-function greet(req,res) {
+function get_weather(req,res) {
     if (req.token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c") {
     response.json({"coord":{"lon":-123.262,"lat":44.5646},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":281.56,"feels_like":281.56,"temp_min":279.46,"temp_max":283.9,"pressure":1032,"humidity":73},"visibility":10000,"wind":{"speed":0.89,"deg":113,"gust":1.34},"clouds":{"all":75},"dt":1642803894,"sys":{"type":2,"id":2040223,"country":"US","sunrise":1642779731,"sunset":1642813576},"timezone":-28800,"id":5720727,"name":"Corvallis","cod":200})
     }
@@ -49,9 +49,11 @@ function greet(req,res) {
 }
 
 //add token for two endpoints
-function get_hello(req, res) {
+function greet(req, res) {
     if (req.token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c") {
-        response.json("Have a nice day!")
+        response.json({
+          "message": "Have a nice day!"
+        });
     }
     else {
       res.sendStatus(401);
